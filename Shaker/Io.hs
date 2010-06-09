1617077bfe32d9e2607f50d6ad6346ef43336fbe
Anthonin Bonnefoy <anthonin.bonnefoy@gmail.com>
2010-06-09 08:28:59 +0200

    Corrections on io : convert to absolute path created listCreatedFiles functions to check modified and created functions

--- Shaker/Io.hs
+++ Shaker/Io.hs
@@ -7,21 +7,32 @@ import System.Time
 import Data.List
 import Shaker.Regex
 
+
+
 listFiles :: FilePath -> [String] -> IO[FilePath]
-listFiles dir ignore = getDirectoryContents dir >>= \res ->
-    return $ filterListWithRegexp res ignore
+listFiles dir ignore = 
+    canonicalizePath dir >>= \curDir ->
+    getDirectoryContents curDir >>= \res ->
+    return $ filterListWithRegexp (convertToFullPath curDir res) ignore
+
+convertToFullPath :: FilePath -> [FilePath] -> [FilePath]
+convertToFullPath absDir lstFp = map (\a-> concat [absDir, "/",a]) lstFp
 
 
 listModifiedFiles :: [(FilePath,ClockTime)] -> IO [(FilePath,ClockTime)]
 listModifiedFiles tupleFpCl = filterM funFilFp tupleFpCl
-  where funFilFp (fp, cl) = getModificationTime fp >>=
-                  \newCl -> return ( newCl /= cl )
+  where funFilFp (fp, cl) = getModificationTime fp >>= \newCl -> 
+          return $ ( newCl > cl )
                          
+listCreatedFiles :: FilePath -> [String] -> [(FilePath,ClockTime)] -> IO[(FilePath,ClockTime)]
+listCreatedFiles dir ignore tupleFpCl = 
+    getCurrentFpCl dir ignore >>= \curFpCl ->
+    return $ deleteFirstsBy (\c p -> fst c == fst p) curFpCl tupleFpCl
+     
+
 getCurrentFpCl :: FilePath -> [String] -> IO [(FilePath, ClockTime) ]
 getCurrentFpCl dir ignore = listFiles dir ignore >>= \lstFp ->
       mapM getModificationTime lstFp >>= \lstCl ->
             zipWithM (\a b-> return (a,b)) lstFp lstCl
                   
-
-
 
--- Shaker/IoTest.hs
+++ Shaker/IoTest.hs
@@ -11,29 +11,32 @@ testListFiles = listFiles "." [] >>= \res ->
         $ length res > 2
 
 testListFilesWithIgnore = listFiles "." ignoreList >>= \res ->
-    assertBool "list files should ignore files given"
-        $ res `intersect` ignoreList == []
-          where ignoreList =["\\.", "\\.\\."]
+    listFiles "." [] >>= \resWoIgnore ->
+    assertEqual "list files should ignore files given"
+         (length res) (length resWoIgnore - 2)
+          where ignoreList =["\\.$", "\\.\\.$"]
 
 testListFilesWithIgnoreAll = listFiles "." [".*"] >>= \res ->
-    assertBool "ignore all files should result in empty list"
-        $ length res == 0 
+    assertEqual "ignore all files should result in empty list"
+        (length res) 0 
 
 testListModifiedFiles = getTemporaryDirectory >>= \tmpDir ->
-      writeFile "test.txt" "testContent"  >>
-            getCurrentFpCl tmpDir ["^\\."] >>= \curList ->
-                  writeFile "test.txt" "testContentBis"  >>
-                        listModifiedFiles curList >>= \newList ->
-                              assertBool "should have at least one file modified"
-                                    $ length newList > 0
-                                       
+  let tmpFile = tmpDir ++"/test.txt" in
+    writeFile tmpFile "testContent"  >>
+    getCurrentFpCl tmpDir ["\\.$"] >>= \curList ->
+    writeFile tmpFile "testContent changed"  >>
+    listModifiedFiles curList >>= \newList ->
+    assertBool "should have at least one file modified"
+      $ length newList > 0
 
 testListCreatedFiles = getTemporaryDirectory >>= \tmpDir ->
-      getCurrentFpCl tmpDir ["^\\."] >>= \curList ->
-            writeFile "test.txt" "testContent"  >>
-                  listModifiedFiles curList >>= \newList ->
-                        assertBool "should have at least one file create"
-                              $ length newList > 0
+  let tmpFile = tmpDir ++"/test.txt" in
+    getCurrentFpCl tmpDir ignoreList >>= \curList ->
+    writeFile tmpFile "testContent"  >>
+    listCreatedFiles tmpDir ignoreList  curList >>= \newList ->
+    assertBool "should have at least one file create"
+      $ length newList > 0
+  where ignoreList = ["\\.$"]
 
 testList = map TestCase [testListFiles,testListFilesWithIgnore, testListFilesWithIgnoreAll, testListModifiedFiles , testListCreatedFiles ]
 
