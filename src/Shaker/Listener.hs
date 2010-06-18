6792e812b33d5437f09d05181b3bb585576a92d9
Anthonin Bonnefoy <anthonin.bonnefoy@gmail.com>
2010-06-18 08:02:30 +0200

    New listener version

--- src/Shaker/Listener.hs
+++ src/Shaker/Listener.hs
@@ -1,14 +1,46 @@
 module Shaker.Listener
- where
+where
 
-import Shaker.Io
+import Control.Monad
+import Control.Concurrent.MVar
 import Control.Concurrent
+import Shaker.Type
+import Shaker.Io
 
-listenerLoop fileListenInfo fileInfo m = 
-  listModifiedAndCreatedFiles fileListenInfo fileInfo >>= \modifiedFiles
-  putMVar m modifiedFiles >>
-  threadDelay 1000 >>
-  listenerLoop fileListenInfo 
+type CurrentFiles = MVar [FileInfo]
+type ModifiedFiles = MVar [FileInfo]
+type Job = MVar FileListenInfo
+
+delay = 5*10^6
+
+listen :: CurrentFiles -> ModifiedFiles -> Job -> IO ()
+listen mC mM mJ = takeMVar mJ  >>= \job ->
+  readMVar mC >>= \curFiles ->
+  readMVar mM >>= \curMod ->
+  updateFileStat mC mM curFiles curMod >>
+  return ()
+
+updateFileStat :: CurrentFiles -> ModifiedFiles -> [FileInfo] -> [FileInfo] -> IO ()
+updateFileStat mC mM curFiles [] = return ()
+updateFileStat mC mM curFiles curMod =
+  swapMVar mC curFiles >>
+  swapMVar mM curMod >>
+  return()
 
 
+initialize :: FileListenInfo -> IO (CurrentFiles, ModifiedFiles)
+initialize fli =
+  newMVar [] >>= \mC ->
+  newMVar [] >>= \mM ->
+  newEmptyMVar >>= \mJ ->
+  (forever $ listen mC mM mJ) >>= \act ->
+  forkIO act >>
+  (forkIO $ schedule fli mJ) >>
+  return (mC,mM)
 
+schedule :: FileListenInfo -> Job -> IO()
+schedule fileListenInfo mJ =
+  putMVar mJ fileListenInfo >>
+  threadDelay delay >>
+  schedule fileListenInfo mJ >>
+  return ()
--- src/Shaker/Type.hs
+++ src/Shaker/Type.hs
@@ -5,12 +5,17 @@ import System.Directory
 import System.Time
 
 -- |Agregate a FilePath with its modification time
-data FileInfo = FileInfo FilePath ClockTime
+data FileInfo = FileInfo FilePath ClockTime 
   deriving (Show,Eq)
 
 hasSameFilePath :: FileInfo -> FileInfo -> Bool
 hasSameFilePath (FileInfo fp1 _) (FileInfo fp2 _) = fp1 == fp2
 
--- | Represents directory to listen with ignore patterns
-data FileListenInfo = FileListenInfo FilePath [String]
+-- | Represents directory to listen 
+data FileListenInfo = FileListenInfo{
+  dir :: FilePath     -- location of the listened directory
+  ,ignore :: [String] -- ignore patterns
+  }
   deriving (Show)
+
+
