9280b058a22fb0b4a026ffc6b6d75b05e05e5f2a
Anthonin Bonnefoy <anthonin.bonnefoy@gmail.com>
2010-06-23 08:09:57 +0200

    quickcheck property compile

--- testsuite/tests/Shaker/ListenerTest.hs
+++ testsuite/tests/Shaker/ListenerTest.hs
@@ -3,15 +3,21 @@ where
 
 import System.Time
 import Control.Concurrent.MVar
+import Control.Monad
 import Shaker.Listener
 import Shaker.Type
 import Test.QuickCheck 
 import Test.QuickCheck.Monadic 
 
-prop_updateFileStat mC mM curF curM = monadicIO test
-  where test = run (updateFileStat mC mM curF curM) >>
-               run (readMVar mC) >>= \mCurF ->
-               assert $  curF == mCurF
+prop_updateFileStat curF curM = 
+  monadicIO (test curF curM)
+
+test curF curM = 
+  run (newMVar []) >>= \mC ->    
+  run (newMVar []) >>= \mM ->    
+  run (updateFileStat mC mM curF curM) >>
+  run (readMVar mC) >>= \mCurF ->
+  assert $  curF == mCurF
 
 instance Arbitrary ClockTime where
    arbitrary = elements [1..1000] >>= \sec ->
