33291472e6231c37c201f0b6a831e555920f0d2f
Anthonin Bonnefoy <anthonin.bonnefoy@gmail.com>
2010-06-23 20:26:52 +0200

    File containing arbitrary instances

---
+++ testsuite/tests/Shaker/Properties.hs
@@ -0,0 +1,31 @@
+module Shaker.Properties
+ where 
+
+import Control.Monad
+import Test.QuickCheck 
+import System.Time
+import Shaker.Type
+
+instance Arbitrary TimeDiff where
+   arbitrary =  TimeDiff `liftM` elements tab
+			 `ap` elements tab
+			 `ap` elements tab
+			 `ap` elements tab
+			 `ap` elements tab
+			 `ap` elements tab
+			 `ap` (elements $ map fromIntegral tab)
+     where tab = [1..10] 
+
+instance Arbitrary ClockTime where
+   arbitrary = TOD `liftM` elements [1..1000]
+		   `ap` elements [1..1000]
+
+instance Arbitrary FileListenInfo where 
+   arbitrary = FileListenInfo `liftM` elements [".",".."]
+			      `ap` arbitrary
+
+instance Arbitrary FileInfo where
+   arbitrary = arbitrary >>= \cl ->
+               elements [".",".."] >>= \ele ->
+               return $ (FileInfo ele cl)
+
