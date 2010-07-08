22efd8b0e6f6e0395c06f844774878dcf81052a5
Anthonin Bonnefoy <anthonin.bonnefoy@gmail.com>
2010-07-08 20:08:24 +0200

    Some cabal corrections

--- src/Shaker/Action/Clean.hs
+++ src/Shaker/Action/Clean.hs
@@ -1,4 +1,12 @@
 module Shaker.Action.Clean
  where
 
+import Shaker.Type
+
+
+{-
+runClean :: Plugin 
+runClean (ShakerInput (CompileInput cfDynFlags _  )  _ _ _) = 
+  hsSourceDirs $ cfDynFlags DynFlags{}
+-} 
 
