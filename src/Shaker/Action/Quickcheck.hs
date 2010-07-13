081baf5605335323d7e370fbaaad9a45f85001c9
Anthonin Bonnefoy <anthonin.bonnefoy@gmail.com>
2010-07-13 08:24:04 +0200

    Initialized quickcheck action

---
+++ src/Shaker/Action/Quickcheck.hs
@@ -0,0 +1,62 @@
+module Shaker.Action.Quickcheck
+ where
+
+import GHC
+import DynFlags 
+import GHC.Paths
+import Shaker.Io
+import Shaker.Type
+import Control.Monad.Trans 
+import Control.Monad.Reader
+{-
+runQuickcheck :: Shaker IO String
+runQuickcheck = do
+        (CompileInput sourceDir targetInput procFlags strflags) <-  asks compileInput 
+        (ListenerInput fli _) <- asks listenerInput 
+        targetFiles <-  lift $ recurseMultipleListFiles fli
+        lift $ defaultErrorHandler defaultDynFlags $ 
+                       runGhc (Just libdir) $ do
+                       dflags <- getSessionDynFlags
+                       (newFlags,_,_) <- parseDynamicFlags dflags (map noLoc strflags)
+	               _ <- setSessionDynFlags $ procFlags $ setSourceAndTarget sourceDir targetInput newFlags
+                       target <- mapM (`guessTarget` Nothing) targetFiles
+                       setTargets target
+        	       _ <- load LoadAllTargets
+                       modSum <- getModSummary $ mkModuleName "B"
+                       p <- parseModule modSum
+                       t <- typecheckModule p
+                       d <- desugarModule t
+                       l <- loadModule d
+                       n <- getNamesInScope
+                       c <- return $ coreModule d
+                       g <- getModuleGraph
+                       mapM showModule g     
+                       print $ showSDoc ( ppr (parsedSource d,"/n-----/n",  typecheckedSource d)        res )
+-}
+
+example = defaultErrorHandler defaultDynFlags $ do
+    runGhc (Just libdir) $ do
+    dflags <- getSessionDynFlags
+    (newFlags,_,_) <- parseDynamicFlags dflags (map noLoc ["-itestsuite/tests","-isrc/","-package ghc"])
+    setSessionDynFlags newFlags
+    target <- guessTarget "Shaker.CliTest" Nothing
+    setTargets [target]
+    load LoadAllTargets
+    modSum <- getModSummary $ mkModuleName "Shaker.CliTest"
+    p <- parseModule modSum
+    t <- typecheckModule p
+    d <- desugarModule t
+    l <- loadModule d
+    n <- getNamesInScope
+    c <- return $ coreModule d
+    g <- getModuleGraph
+    mapM showModule g     
+    return $ (parsedSource d,"/n-----/n",  typecheckedSource d)
+
+setSourceAndTarget :: [String] -> String ->DynFlags -> DynFlags
+setSourceAndTarget sources target dflags = dflags{
+    importPaths = sources
+    ,objectDir = Just target
+    ,hiDir = Just target
+  }
+
