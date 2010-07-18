036b7040717dd5c3013ea48498bf88d9f0c179ab
Anthonin Bonnefoy <anthonin.bonnefoy@gmail.com>
2010-07-18 16:59:49 +0200

    Added test : Error when the haskell source directory is the current directory

--- testsuite/tests/Shaker/Cabal/CabalInputTest.hs
+++ testsuite/tests/Shaker/Cabal/CabalInputTest.hs
@@ -1,11 +1,21 @@
 module Shaker.Cabal.CabalInputTest
  where 
 
+import System.Directory
+import System.FilePath
 import Shaker.Action.Compile
 import Test.HUnit
-import System.Directory
-import Shaker.Config 
 import Control.Monad.Reader
-import Shaker.Type
+import Shaker.Cabal.CabalInput
+import Distribution.Simple.Configure 
 
+testProjectWithLocalSource :: Test
+testProjectWithLocalSource = TestCase $ do
+  let directory = "testsuite/tests/resources/noSourceConfig"
+  lbi <- getPersistBuildConfig $ directory </> "dist"
+  let shIn = cabalInfosToShakerInput $ localBuildInfoToCabalInfoList lbi
+  setCurrentDirectory ""
+  runReaderT runCompile shIn 
+  assertBool ("") False
+  
 
