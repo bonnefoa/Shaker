ef4e6c72af94f96d832d39b50ba2330395b27525
Anthonin Bonnefoy <anthonin.bonnefoy@gmail.com>
2010-08-03 20:32:48 +0200

    Before using monad reader

--- src/Shaker/Action/Compile.hs
+++ src/Shaker/Action/Compile.hs
@@ -14,19 +14,19 @@ import Control.Monad.Reader
 
 -- |Run haskell compilation on given file input 
 runCompile :: Plugin
-runCompile = asks compileInputs >>= mapM runSingleCompileInput >> return ()
+runCompile = asks compileInputs >>= mapM (lift . runSingleCompileInput )  >> return ()
  
 runFullCompile :: Plugin
 runFullCompile = do
   cpIn <- mergeCompileInputsSources 
   cfFlList <- lift $ constructCompileFileList cpIn
-  runSingleCompileInput $ ( removeFileWithMain cfFlList . setAllHsFilesAsTargets cfFlList ) cpIn
+  lift $ runSingleCompileInput $ ( removeFileWithMain cfFlList . setAllHsFilesAsTargets cfFlList ) cpIn
 
-runSingleCompileInput :: CompileInput -> Shaker IO()
+runSingleCompileInput :: CompileInput -> IO()
 runSingleCompileInput cplInp = do
-        lift $ putStrLn $ concat ["   --------- ", cfDescription cplInp," ---------"]
-        lift $ putStrLn $ concat ["   --------- ", "Compiling target : "++ show (cfTargetFiles cplInp) ," ---------"]
-        _ <- lift $ defaultErrorHandler defaultDynFlags $ 
-                       runGhc (Just libdir) $ ghcCompile cplInp 
+        putStrLn $ concat ["   --------- ", cfDescription cplInp," ---------"]
+        putStrLn $ concat ["   --------- ", "Compiling target : "++ show (cfTargetFiles cplInp) ," ---------"]
+        _ <- defaultErrorHandler defaultDynFlags $ 
+                    runGhc (Just libdir) $ ghcCompile cplInp 
         return ()
 
