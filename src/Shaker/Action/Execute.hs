750349b565e180046b803c973a2edd3bc6f120bd
Anthonin Bonnefoy <anthonin.bonnefoy@gmail.com>
2010-08-06 08:31:01 +0200

    implement execute task

--- src/Shaker/Action/Execute.hs
+++ src/Shaker/Action/Execute.hs
@@ -2,26 +2,21 @@ module Shaker.Action.Execute
  where
 
 import Shaker.Type
-import Shaker.SourceHelper
-import GHC.Paths
-import GHC
+import Shaker.Reflexivite
 import Control.Monad.Reader
+import Control.Arrow
 
 runExecute :: Plugin
 runExecute = do
-  cpList <- asks compileInputs 
-  let cpIn = mergeCompileInputsSources cpList 
-  lift $ runGhc (Just libdir) $ do 
-            _ <- ghcCompile cpIn
-            modSummaries <- getModuleGraph
-            return ()
-  return ()
+  arg <- asks argument
+  launchFunction arg
 
-launchFunction :: (Maybe String) -> Plugin 
+launchFunction :: Maybe String -> Plugin 
 launchFunction Nothing = lift $ putStrLn "No action to execute. Give an argument of ModuleName.functionName. The function should be of type IO()"
-launchFunction (Just actStr) = lift $ putStrLn "Ga "
-  where (mod, fun) = parseModuleAndAction actStr
+launchFunction (Just actStr) = runFunction runnableFunction
+  where runnableFunction = parseModuleAndAction actStr
 
-parseModuleAndAction :: String -> (String,String)
-parseModuleAndAction actStr = (moduleStr, functionStr)
-  where (functionStr, moduleStr) = ( \(a,b) -> (a,reverse . tail $ b) ) . span (/= '.') . reverse $ actStr
+parseModuleAndAction :: String -> RunnableFunction
+parseModuleAndAction actStr = RunnableFunction moduleStr functionStr
+  where (functionStr, moduleStr) =  second (reverse . tail )  . span (/= '.') . reverse $ actStr
+
