cdd8a7310a7ac68787eaa38bd495f5f58baade11
Anthonin Bonnefoy <anthonin.bonnefoy@gmail.com>
2010-11-22 20:22:41 +0100

    Removed test hooks and test configuration

--- Setup.hs
+++ Setup.hs
@@ -6,11 +6,4 @@ import Distribution.Simple.LocalBuildInfo
 import System
 import System.FilePath
 
-main = defaultMainWithHooks hooks
-  where hooks = simpleUserHooks { runTests = runTests' }
-
-
-runTests' :: Args -> Bool -> PackageDescription -> LocalBuildInfo -> IO ()
-runTests' _ _ _ lbi = system testprog >> return ()
-  where testprog = buildDir lbi </> "test" </> "test" 
-
+main = defaultMain
