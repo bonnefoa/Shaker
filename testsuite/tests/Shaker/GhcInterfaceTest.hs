157f706e9ba6695a00144eaad78a9dcfaf65e74b
Anthonin Bonnefoy <anthonin.bonnefoy@gmail.com>
2010-11-22 12:54:49 +0100

    Corrected test list needed packages

--- testsuite/tests/Shaker/GhcInterfaceTest.hs
+++ testsuite/tests/Shaker/GhcInterfaceTest.hs
@@ -14,5 +14,5 @@ testListNeededPackages = do
   let cpIn = mempty {compileInputCommandLineFlags = ["-hide-all-packages"]}
   let shIn = defaultInput { shakerCompileInputs = [cpIn]  }
   list_needed_imports <- runReaderT getListNeededPackages shIn
-  any (isPrefixOf "bytestring") list_needed_imports @? show list_needed_imports
+  any (isPrefixOf "haskeline") list_needed_imports @? show list_needed_imports
 
